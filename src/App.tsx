d9995801a6bdea47b91ab60d6e27ee9b7ce26b6b
Ilmi <ilmirasyidah.sekawanmedia@gmail.com>
2024-07-12 10:51:02 +0700

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <div className="bg-purple-500"></div>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
